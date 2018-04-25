8ae65ca0e9deb9e7f9e96927356b04add3073b8e
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-04-25 23:05:39 +0545

    Replace the chain Functionality

--- blockchain.js
+++ blockchain.js
@@ -22,5 +22,16 @@ class Blockchain {
         }
         return true;
     }
+
+    replaceChain(newChain) {
+        if (newChain.length <= this.chain.length) {
+            console.log('Received chain is not longer than the current chain.');
+            return;
+        } else if (!this.isValidChain(newChain)) {
+            console.log('The received chain is not valid.');
+        }
+        console.log('REplacing blockchain with the new chain.');
+        this.chain = newChain;
+    }
 }
 module.exports = Blockchain;
