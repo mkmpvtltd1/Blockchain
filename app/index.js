5a855f33aede1de2c3e43b64bb76a70dbfd6ff0d
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-04-27 06:32:37 +0545

    Creating the Websocket Server

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
+
 app.use(bodyParser.json());
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
@@ -17,3 +20,4 @@ app.post('/mine', (req, res) => {
     res.redirect('/blocks');
 });
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -14,10 +14,21 @@ class P2pServer {
             port: P2P_PORT
         });
         server.on('connection', socket => this.connectSocket(socket));
+        this.connectToPeers();
         console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
     }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            const socket = new WebSocket(peer);
+            socket.on('open',()=> this.connectSocket(socket));
+        });
+    }
+
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket Connected');
     }
 }
+
+module.exports =P2pServer;
