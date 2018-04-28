56558ea2977dcad3bf494f3e8bbf22c380afa346
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-04-28 09:31:58 +0545

    test Prrof of work Function

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,8 @@
 const Block = require('./block');
+const {
+    DIFFICULTY
+} = require('../config');
+
 describe('Block', () => {
     let data, lastBlock, block;
     beforeEach(() => {
@@ -6,11 +10,17 @@ describe('Block', () => {
         lastBlock = Block.genesis();
         block = Block.mineBlock(lastBlock, data);
     });
+
     it('sets the `data` to match the input', () => {
         expect(block.data).toEqual(data);
     });
+
     it('sets the `lastHash` to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
 
+    });
+
+    it('generate a hash that matches the difficulty ', () => {
+        expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
     });
 });
